5fb0bb0d667f23b96a9c2e0c67bd34c88fd1dd12
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-04-08 22:04:11 +0200

    Move client and project names to config files

--- script.rb
+++ script.rb
@@ -5,6 +5,14 @@ def readable_time(time)
   "5 hours, 17 minutes and 42 seconds"
 end
 
+def client_name
+  File.read('.client_name.secret')
+end
+
+def project_name
+  File.read('.project_name.secret')
+end
+
 def build_calendar_entry
   toggl = Toggl::Timer.new
   toggl.print_config
@@ -14,15 +22,15 @@ def build_calendar_entry
   one_day = 86400
   yesterday = time - one_day
   work_start_time_yesterday = toggl.get_work_start_time(yesterday)
-  half_an_hour = 1800
-  title = "Work for Rebased"
+  one_hour = 3600
+  title = "Work for #{client_name}"
   work_time_yesterday = toggl.get_total_work_time(yesterday)
   human_readable_work_time_yesterday = readable_time(work_time_yesterday)
-  description = "Time: #{human_readable_work_time_yesterday}\nProject: Harmonogram"
+  description = "Time: #{human_readable_work_time_yesterday}\nProject: #{project_name}"
 
   {
     start: work_start_time_yesterday,
-    duration: half_an_hour,
+    duration: one_hour,
     title: title,
     description: description,
   }
