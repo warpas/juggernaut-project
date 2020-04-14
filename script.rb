0de910bf1e05b9d58ca4cd979509cea2e6db6845
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-04-14 22:15:50 +0200

    Add support for multiple toggl time entries

--- script.rb
+++ script.rb
@@ -1,14 +1,24 @@
 require_relative 'Toggl'
 require_relative 'Google/Calendar'
 
+# TODO: Add unit tests.
+
 def readable_time(time)
-  "5 hours, 17 minutes and 42 seconds"
+  # TODO: move parsing and other helpers to a different file.
+  time_integer = time.to_i
+  time_in_seconds = time_integer/1000
+  hours = time_in_seconds/3600
+  minutes_in_seconds = time_in_seconds % 3600
+  minutes = minutes_in_seconds/60
+  seconds = minutes_in_seconds % 60
+  "#{hours} hours, #{minutes} minutes and #{seconds} seconds"
 end
 
 def formatted_date(date)
   "#{date.year}-#{date.month}-#{date.day}"
 end
 
+# TODO: remove these and streamline configuration.
 def client_name
   File.read('.client_name.secret')
 end
@@ -17,7 +27,9 @@ def project_name
   File.read('.project_name.secret')
 end
 
+# TODO: add a function to creat a week summary (total time logged) event on Monday morning
 def build_calendar_entry_from_x_days_ago(days_ago)
+  # TODO: move what should be in Toggl API to toggl file.
   puts "\ninitiating Toggl integration"
 
   time = Time.now
@@ -26,27 +38,32 @@ def build_calendar_entry_from_x_days_ago(days_ago)
 
   toggl = Toggl::Timer.new(formatted_date(date_time))
   toggl.print_config
-  work_start_time = toggl.get_work_start_time(date_time)
-  title = "Work for #{client_name}"
-  duration = toggl.get_total_time
-  total_work_time = toggl.get_total_work_time(date_time)
-  human_readable_total_work_time = readable_time(total_work_time)
-  description = "Time: #{human_readable_total_work_time}\nProject: #{project_name}"
+  detailed_report = toggl.report_details
 
-  {
-    start: work_start_time,
-    duration: duration,
-    title: title,
-    description: description,
-  }
+  puts "\nBuilding the list of events"
+  total_time_logged = detailed_report['total_grand']
+  # TODO: handle report without entries
+  detailed_report['data'].map do |entry|
+    {
+      start: entry['start'],
+      end: entry['end'],
+      title: entry['description'],
+      duration: entry['dur'],
+      description: "Duration: #{readable_time(entry['dur'])}\nClient: #{entry['client']}\nProject: #{entry['project']}\nTotal time logged today: #{readable_time(total_time_logged)}",
+    }
+  end
 end
 
-def add_to_calendar(entry)
+def add_to_calendar(entry_list)
+  # TODO: move what should be in calendar API to calendar file.
   puts "\ninitiating Google Calendar integration"
   calendar = Google::Calendar.new
   calendar.fetch_next_events(5)
-  calendar.add_work_entry(entry)
+  entry_list.each do |entry|
+    calendar.add_work_entry(entry)
+  end
 end
 
-prepared_entry = build_calendar_entry_from_x_days_ago(6)
-add_to_calendar(prepared_entry)
+# TODO: change the way date is given. Ideally a GUI with a date picker. For now it could just be date given as a command line argument.
+prepared_entry_list = build_calendar_entry_from_x_days_ago(1)
+add_to_calendar(prepared_entry_list)
