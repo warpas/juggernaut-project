3d3e67ef8816fad472842ca4a7ed86757f9f4067
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-05-20 22:58:20 +0200

    Fix RescueTime call to get a response

--- rescuetime_playground.rb
+++ rescuetime_playground.rb
@@ -9,10 +9,12 @@ end
 key = connect_to_rescue_time(api_key_source: ".rt_apikey.secret.json")
 puts key
 
-base_path = "https://www.rescuetime.com/anapi/daily_summary_feed"
-request_path = "#{base_path}/key=#{key}&format=json"
+base_path = "https://www.rescuetime.com/anapi/"
+daily_summary_feed_request_path = "#{base_path}daily_summary_feed?key=#{key}&format=json"
 
-puts request_path
+data_request_path = "#{base_path}data?key=#{key}&perspective=interval&restrict_kind=productivity&interval=hour&restrict_begin=2020-01-01&restrict_end=2020-01-01&format=json"
+
+puts daily_summary_feed_request_path
 request_adapter = Requests::Adapter.new
-result = request_adapter.get_request(request_path, [{}])
+result = request_adapter.get_request(daily_summary_feed_request_path, [{}])
 puts result
