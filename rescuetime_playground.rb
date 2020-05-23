68e5a9ac9b84e0a6a9f9bfa5fab5561e74e60177
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-05-23 22:31:37 +0200

    Investigate RescueTime's data endpoint

--- rescuetime_playground.rb
+++ rescuetime_playground.rb
@@ -24,18 +24,19 @@ def build_calendar_event(daily_summary:)
 end
 
 key = connect_to_rescue_time(api_key_source: ".rt_apikey.secret.json")
-puts key
 
 base_path = "https://www.rescuetime.com/anapi/"
 daily_summary_feed_request_path = "#{base_path}daily_summary_feed?key=#{key}&format=json"
 
-data_request_path = "#{base_path}data?key=#{key}&perspective=interval&restrict_kind=productivity&interval=hour&restrict_begin=2020-01-01&restrict_end=2020-01-01&format=json"
+data_request_path = "#{base_path}data?key=#{key}&perspective=interval&restrict_kind=category&resolution_time=minute&interval=hour&restrict_begin=2020-05-23&restrict_end=2020-05-23&format=json"
 
 puts daily_summary_feed_request_path
 request_adapter = Requests::Adapter.new
 result = request_adapter.get_request(daily_summary_feed_request_path, [{}])
+result_data = request_adapter.get_request(data_request_path, [{}])
+puts "result_data = #{JSON.parse(result_data[:body])}"
+
 parsed_response = JSON.parse(result[:body])
-puts parsed_response.length
 prepared_entry_list = parsed_response.map do |daily_summary|
   build_calendar_event(daily_summary: daily_summary)
 end
