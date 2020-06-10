a60f32e3cff86c3347c02a23d826958b12b1bfce
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-06-10 21:15:23 +0200

    Add basic test for time formatting

--- spec/toggl/google_calendar_adapter_spec.rb
+++ spec/toggl/google_calendar_adapter_spec.rb
@@ -4,6 +4,7 @@ describe Toggl::GoogleCalendarAdapter do
   subject { described_class.new }
   let(:date) { Date.parse("2020-06-07") }
   let(:empty_report) { {"total_grand" => 0, "data" => []} }
+  let(:full_work_week_report) { {"total_grand" => 144_816_000, "data" => []} }
 
   describe "#build_entry_list_from" do
   end
@@ -20,6 +21,12 @@ describe Toggl::GoogleCalendarAdapter do
     it "should have the right title" do
       expect(subject.build_weekly_summary(report: empty_report, report_day: date).first).to include(
         title: "Last week summary"
+      )
+    end
+
+    it "should format time into hours/minutes/seconds" do
+      expect(subject.build_weekly_summary(report: full_work_week_report, report_day: date).first).to include(
+        description: "⏱Total time logged last week:\n40 hours, 13 minutes and 36 seconds\n"
       )
     end
   end
