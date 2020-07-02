8c784314c0ad5957c8523fa6c72b0edaee8bd69b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-02 23:16:49 +0200

    Add auth headers through a separate class static method

---
+++ libs/toggl/auth.rb
@@ -0,0 +1,26 @@
+module Toggl
+  require "base64"
+  require "json"
+
+  class Auth
+    def self.headers
+      [{key: "Authorization", value: basic_auth_token}]
+    end
+
+    private
+
+    def self.get_json_from_file(file_path)
+      file = File.read(file_path)
+      JSON.parse(file)
+    end
+
+    def self.basic_auth_token
+      "Basic " + Base64.encode64(api_token + ":api_token").strip
+    end
+
+    def self.api_token
+      config = self.get_json_from_file("libs/toggl/config.secret.json")
+      config["tokens"]["api"]
+    end
+  end
+end
--- libs/toggl/time_entry.rb
+++ libs/toggl/time_entry.rb
@@ -1,15 +1,36 @@
 module Toggl
   require_relative "../requests"
+  require_relative "auth"
 
   class TimeEntry
-    def initialize(project_id:, start:)
+    def initialize(project_id:, start: Time.now.to_s)
+      @project_id = project_id
       @request_adapter = Requests::Adapter.new
     end
 
     def save
-      address = "https://www.toggl.com/api/v8/time_entries"
-      body = '{"time_entry":{"description":"Test timer","tags":["work"],"duration":1200,"start":"2020-07-01T07:58:58.000Z","pid":123,"created_with":"curl"}}'
-      response = @request_adapter.post_request(address, headers, body)
+      response = @request_adapter.post_request(time_entries_url, headers, body)
+      puts response
+      response
+    end
+
+    def split
+    end
+
+    private
+
+    attr_reader :project_id
+
+    def time_entries_url
+      "https://www.toggl.com/api/v8/time_entries"
+    end
+
+    def headers
+      Toggl::Auth.headers
+    end
+
+    def body
+      "{\"time_entry\":{\"description\":\"Test timer\",\"tags\":[\"work\"],\"duration\":1200,\"start\":\"2020-07-02T07:58:58.000Z\",\"pid\":#{project_id},\"created_with\":\"Juggernaut\"}}"
     end
   end
 end
