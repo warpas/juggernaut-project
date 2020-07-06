719fa6f56c418249f815a481740ca7a5062e769c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-06 23:06:01 +0200

    Add prototype split method to TimeEntry

--- libs/toggl/time_entry.rb
+++ libs/toggl/time_entry.rb
@@ -16,12 +16,33 @@ module Toggl
       response
     end
 
-    def split
+    def self.split(entry_to_split:, breakpoint:)
+      # TODO: change it to an instance method. Add TimeEntry.find(id)
+      puts "entry_to_split.start = #{entry_to_split["start"]}"
+      puts "entry_to_split.end = #{entry_to_split["end"]}"
+      puts "entry_to_split.duration = #{entry_to_split["dur"]}"
+
+      pre_entry = copy_with_changes(copy: entry_to_split, change: {end: breakpoint})
+      post_midnight_entry = copy_with_changes(copy: entry_to_split, change: {start: breakpoint})
+      remove(entry_to_split)
     end
 
     private
 
     attr_reader :project_id
+
+    def self.copy_with_changes(copy:, change:)
+      # TODO: change it to an instance method
+
+      puts "entry_to_split = #{copy}"
+      puts "change = #{change}"
+    end
+
+    def self.remove(entry)
+      # TODO: change it to an instance method
+
+      puts "entry_to_remove = #{entry}"
+    end
 
     def time_entries_url
       "https://www.toggl.com/api/v8/time_entries"
