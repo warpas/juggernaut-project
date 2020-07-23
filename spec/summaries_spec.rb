2348940b41519f203dbea4b1dbcb7acb028181ec
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-23 17:33:57 +0200

    Reflect Daily Summary hierarchy change in tests

--- spec/summaries_spec.rb
+++ spec/summaries_spec.rb
@@ -2,25 +2,29 @@ require "spec_helper"
 
 # TODO: everything here needs better names
 # TODO: everything here needs to be implemented. For now it's an exercise in public interface design
-describe Summaries::Daily do
-  subject { described_class }
+describe Summaries::Daily::Trends do
+  subject { described_class.new(cumulative: summary_report, detailed: detailed_report) }
+
   let(:before_logging) { Date.parse("2019-07-17") }
   let(:friday) { Date.parse("2020-07-17") }
   let(:empty_trends) { [["2019-07-17", "0", "0", "0", "0", "0", "0", "0", "0"]] }
   let(:trends) { [["2020-07-17", "146", "0", "149", "149", "296", "2", "543", "1"]] }
   let(:todays_trends) { [[Date.today.to_s, "0", "0", "0", "0", "0", "0", "0", "0"]] }
+  let(:summary_report) { []
+  }
+  let(:detailed_report) { []}
 
   describe "#build_trends_for" do
     it "should generate a trend data row based on Time Log data for the day" do
-      expect(subject.build_trends_for(date: friday)).to eq(trends)
+      expect(subject.build(date: friday)).to eq(trends)
     end
 
     it "should return an trends row for today if no date is given" do
-      expect(subject.build_trends_for()).to eq(todays_trends)
+      expect(subject.build()).to eq(todays_trends)
     end
 
     it "should return an empty trends row if there was no data on the date given" do
-      expect(subject.build_trends_for(date: before_logging)).to eq(empty_trends)
+      expect(subject.build(date: before_logging)).to eq(empty_trends)
     end
   end
 end
