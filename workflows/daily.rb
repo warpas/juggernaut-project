44eedf2778655ae6cc26548a06725dd1a5249bff
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-24 19:14:40 +0200

    Add looping to workflow scripts

--- workflows/daily.rb
+++ workflows/daily.rb
@@ -1,6 +1,19 @@
-load "scripts/split_toggl_midnight_timers.rb"
-load "scripts/send_toggl_to_calendar.rb"
-load "scripts/populate_calendar.rb"
-load "scripts/daily_creative_report.rb"
-load "scripts/daily_work_report.rb"
-load "scripts/daily_trends_data_export.rb"
+daily_scripts =
+  [
+    "scripts/split_toggl_midnight_timers.rb",
+    "scripts/send_toggl_to_calendar.rb",
+    "scripts/populate_calendar.rb",
+    "scripts/daily_creative_report.rb",
+    "scripts/daily_work_report.rb",
+    "scripts/daily_trends_data_export.rb",
+  ]
+
+iterator = 1
+script_count = daily_scripts.count
+daily_scripts.each do |script|
+  puts "\n⚙️  Running script number #{iterator} / #{script_count}\n"
+  load script
+  puts "\n✅  Script number #{iterator} / #{script_count} ran successfully\n"
+  iterator += 1
+end
+puts "\n✅  All scripts within the workflow ran successfully!!"
--- workflows/weekly.rb
+++ workflows/weekly.rb
@@ -1,1 +1,14 @@
-load "scripts/weekly_work_report.rb"
+weekly_scripts =
+  [
+    "scripts/weekly_work_report.rb",
+  ]
+
+iterator = 1
+script_count = weekly_scripts.count
+weekly_scripts.each do |script|
+  puts "\n⚙️  Running script number #{iterator} / #{script_count}\n"
+  load script
+  puts "\n✅  Script number #{iterator} / #{script_count} ran successfully\n"
+  iterator += 1
+end
+puts "\n✅  All scripts within the workflow ran successfully!!"
