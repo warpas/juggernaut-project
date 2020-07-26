d148fef30f392059e490c5843c68752f8531dfc2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-26 22:04:06 +0200

    Add interface context tests

--- libs/interface/command_line.rb
+++ libs/interface/command_line.rb
@@ -2,13 +2,11 @@ require "date"
 
 module Interface
   class CommandLine
-    def initialize(args:)
+    def initialize(args: [])
       @runtime_args = args
     end
 
     def get_runtime_date(default: Date.today)
-      puts "@runtime_args = #{@runtime_args}"
-      puts "default = #{default}"
       date = default.to_s
       @runtime_args.each do |cl_argument|
         split_params = cl_argument.split("=")
