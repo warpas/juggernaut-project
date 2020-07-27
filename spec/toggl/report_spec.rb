9e64e261bb22cbe269393dc63a55abdc8d700e98
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-07-27 20:40:55 +0200

    Disable an unfinished test

--- spec/toggl/report_spec.rb
+++ spec/toggl/report_spec.rb
@@ -1,11 +1,12 @@
 require "spec_helper"
 
+# TODO: make sure these test doesn't send any external requests
 describe Toggl::Report do
   # RSpec.describe Toggl::Connection do
   subject { described_class.new(date) }
   let(:date) { "2020-04-18" }
 
-  it "should" do
+  xit "should" do
     expect(subject.start_date).to equal(date)
   end
 end
