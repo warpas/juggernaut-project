87e9921d409c3aeea3e9ab598574e49c46326656
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-08-03 22:34:04 +0200

    Add CLI to weekly report script

--- scripts/weekly_report.rb
+++ scripts/weekly_report.rb
@@ -1,13 +1,12 @@
-require_relative "../libs/command_line"
+require_relative "../libs/interface/command_line"
 require_relative "../libs/toggl/report"
 require_relative "../libs/toggl/google_calendar_adapter"
 require_relative "../libs/google/calendar"
 require_relative "../date_time_helper"
 require "date"
 
-def build_weekly_summary(date_string)
+def build_weekly_summary(date)
   # TODO: I want this to compare total time with last week
-  date = Date.parse(date_string)
   week_start = DateTimeHelper.get_week_start(date)
   week_end = DateTimeHelper.get_next_closest_sunday(date)
   toggl = Toggl::Report.new(week_start, week_end)
@@ -15,16 +14,13 @@ def build_weekly_summary(date_string)
   adapter.build_weekly_summary(report: toggl.report_summary, report_day: week_end + 1)
 end
 
-puts "\n⌨️  Running weekly_report script"
-date = CommandLine.get_date_from_command_line(ARGV)
+def cl_date
+  cli = Interface::CommandLine.new(args: ARGV)
+  cli.get_runtime_date(default: Date.today - 7)
+end
 
-prepared_entry_list =
-  if date.empty?
-    last_week = Date.today - 7
-    build_weekly_summary(last_week.to_s)
-  else
-    build_weekly_summary(date)
-  end
+puts "\n⌨️  Running weekly_report script"
+prepared_entry_list = build_weekly_summary(cl_date)
 
 puts "\ninitiating Google Calendar integration"
 calendar = Google::Calendar.new
