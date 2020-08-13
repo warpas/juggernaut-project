54a6332668a74948fe7f83c49dd00d1ed8c56d21
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-08-13 19:45:05 +0200

    Add test scaffold for Workflows Runner

--- spec/workflows/runner_spec.rb
+++ spec/workflows/runner_spec.rb
@@ -3,7 +3,17 @@ require "spec_helper"
 describe Workflows::Runner do
   subject { described_class }
 
-  describe "#run" do
+  describe "#new" do
     it { should respond_to(:new) }
+
+    xit "should test the interesting stuff"
+  end
+
+  describe "#start" do
+    subject { described_class.new }
+
+    it { should respond_to(:start) }
+
+    xit "should test the interesting stuff"
   end
 end
