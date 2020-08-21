06f2e48845c322226a5e39b7554df92cb02ca8e7
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-08-21 17:42:39 +0200

    Add Availability to WorkTimeReporter

--- libs/analysis/work_time_reporter.rb
+++ libs/analysis/work_time_reporter.rb
@@ -1,4 +1,5 @@
 require_relative "../toggl/report"
+require_relative "../interface/command_line"
 require_relative "../../date_time_helper"
 require "date"
 
@@ -9,8 +10,12 @@ module Analysis
       reporter.build_report(date: Date.today)
     end
 
+    def initialize
+      @cli = Interface::CommandLine.new(args: ARGV)
+    end
+
     def build_report(date:)
-      client_name = "Client"
+      client_name = @cli.get_runtime_argument(name: "client", default: "Client")
       toggl = Toggl::Report.new(date)
 
       filtered_report = toggl.report_summary["data"].select do |category|
@@ -20,9 +25,47 @@ module Analysis
         category["time"]
       end
       time_in_milliseconds = time_list.sum
+      available_hours = get_availability_time_in_hours(date: date)
+      time_to_halfway = calculate_time_to_halfway_point(time_in_milliseconds, available_hours)
 
-      formatted_time = DateTimeHelper.readable_duration(time_in_milliseconds)
-      puts formatted_time
+      print_availability_message(available_hours)
+      print_work_time_message(time_in_milliseconds)
+      print_halfway_message(time_to_halfway)
+    end
+
+    private
+
+    attr_reader :cli
+
+    def calculate_time_to_halfway_point(time, available_hours)
+      halfway_point = available_hours / 2
+      halfway_point_milliseconds = halfway_point * 3600 * 1000
+      halfway_point_milliseconds - time
+    end
+
+    def get_availability_time_in_hours(date:)
+      @cli.get_runtime_argument(name: "availability", default: 8).to_i
+    end
+
+    def print_availability_message(time)
+      available_milliseconds = time * 3600 * 1000
+      formatted_availability = DateTimeHelper.readable_duration(available_milliseconds)
+      puts "Availability today: #{formatted_availability}"
+    end
+
+    def print_work_time_message(time)
+      formatted_time = DateTimeHelper.readable_duration(time)
+      puts "Worked today for: #{formatted_time}"
+
+    end
+
+    def print_halfway_message(time_left)
+      if time_left > 0
+        formatted_halfway = DateTimeHelper.readable_duration(time_left)
+        puts "Time left until the halfway point today: #{formatted_halfway}"
+      else
+        puts "🎉🎉🎉   Halfway point reached!!  🎉🎉🎉"
+      end
     end
   end
 end
--- libs/interface/command_line.rb
+++ libs/interface/command_line.rb
@@ -17,6 +17,17 @@ module Interface
       Date.parse(date)
     end
 
+    def get_runtime_argument(name: "argument", default: "default")
+      argument = default
+      @runtime_args.each do |cl_argument|
+        split_params = cl_argument.split("=")
+        if split_params.first == "--#{name}" && split_params.length == 2
+          argument = split_params.last
+        end
+      end
+      argument
+    end
+
     def self.log(string)
       puts string
     end
