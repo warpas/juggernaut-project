00b5230950e174347d7ef57db09c29c68bf471e2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-10-06 17:44:40 +0200

    Clean up storage.trends

--- lib/storage/context.rb
+++ lib/storage/context.rb
@@ -4,6 +4,6 @@ require_relative 'trends'
 
 module Storage
   def self.trends_destination
-    Storage::Trends.destination
+    Storage::Trends.new.destination
   end
 end
--- lib/storage/trends.rb
+++ lib/storage/trends.rb
@@ -8,17 +8,10 @@ module Storage
       @destination = spreadsheet_adapter.connect(file_id: 'trends', default_range: 'Data!A:I')
     end
 
-    def self.destination
-      # new.destination
-      spreadsheet_adapter.connect(file_id: 'trends', default_range: 'Data!A:I')
-    end
-
     private
 
     def spreadsheet_adapter
       Integrations::Google::Sheets
     end
-
-    private_class_method :spreadsheet
   end
 end
