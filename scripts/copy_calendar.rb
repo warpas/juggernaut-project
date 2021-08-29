8b5edca35d837ddc7c846b1f628ace65847df48a
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-08-29 13:35:45 +0200

    Apply rubocop to copy calendar

--- scripts/copy_calendar.rb
+++ scripts/copy_calendar.rb
@@ -1,10 +1,10 @@
-require_relative "../lib/google/calendar"
+require_relative '../lib/google/calendar'
 
 # Copy all calendars with names starting with "Exercise -" to the defined calendar
-calendar_object = Google::Calendar.new(calendar_name: "exercise")
-list = calendar_object.list_calendars.select { |calendar|
-  calendar[:name].start_with?("Exercise -")
-}
+calendar_object = Google::Calendar.new(calendar_name: 'exercise')
+list = calendar_object.list_calendars.select do |calendar|
+  calendar[:name].start_with?('Exercise -')
+end
 
 list.each do |calendar|
   fetched = calendar_object.fetch_all_events(calendar[:id])
