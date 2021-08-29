11b73530d1712acee4ac6f5c3e4434fc0942de17
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-08-29 13:30:58 +0200

    Add calendar count workflow implementation part

--- lib/workflows/context.rb
+++ lib/workflows/context.rb
@@ -1,3 +1,4 @@
+require_relative 'calendar_count'
 require_relative 'daily'
 require_relative 'weekly'
 
@@ -8,5 +9,10 @@ module Workflows
 
   def self.run_weekly_workflow
     Workflows::Weekly.run
+  end
+
+  def self.run_calendar_count_workflow
+    puts 'run_calendar_count_workflow'
+    Workflows::CalendarCount.run
   end
 end
