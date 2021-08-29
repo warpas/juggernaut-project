2b22d7aeced953d9da4396030c3071c3c68a77d2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-08-29 14:17:05 +0200

    Add candidate for rough tests for todoist

--- spec/integrations/todoist/tasks_spec.rb
+++ spec/integrations/todoist/tasks_spec.rb
@@ -7,4 +7,12 @@ describe Integrations::Todoist::Tasks do
     described_class.new
   end
   it { should respond_to(:list) }
+  # it { should respond_to(:filter) }
+
+  # TODO: make sure these test don't send any external requests
+  # describe '#with' do
+  #   it 'should should return the total time logged (in seconds)' do
+  #     expect(subject.recorded_seconds).to eq(reported_seconds)
+  #   end
+  # end
 end
